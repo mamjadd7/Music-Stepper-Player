e59e64709e1941bc2c4ff2e032296505aca20dd0
MAMJADJr <MAMJADJr@gmail.com>
2023-08-28 11:13:09 +0500

    SignUp activity Design complete

--- app/src/main/java/com/example/musicsteeeper/ui/MainActivity.kt
+++ app/src/main/java/com/example/musicsteeeper/ui/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.musicsteeeper.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.musicsteeeper.R
 //done
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logOut = findViewById<TextView>(R.id.buttonSignUp)
+        val login = findViewById<TextView>(R.id.buttonLogin)
+
+        login.setOnClickListener {
+            startActivity(Intent(this, HomeExpandedActivity::class.java))
+        }
+
+
+        logOut.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
